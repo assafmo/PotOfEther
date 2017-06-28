a4fd3de43ec1106a8e908671890199073784dd3f
assafmo <assaf.morami@gmail.com>
2017-06-28 21:59:31 +0300

    contract first draft

--- contracts/PotOfGold.sol
+++ contracts/PotOfGold.sol
@@ -7,7 +7,7 @@ contract PotOfGold {
 
         address[] players;
         
-        uint lastPlayerBlockNumber; // uint8(uint256(sha3(block.blockhash(lastJoinerBlockNumber+1)))) % 3 == loser index 
+        uint lastPlayerBlockNumber; // uint8(uint256(sha3(block.blockhash(lastPlayerBlockNumber+1)))) % 3 == loser index 
         
         address loser;
     }
@@ -71,13 +71,53 @@ contract PotOfGold {
         }
     }
 
+    /*function leavePot(string name) {
+        Pot pot = nameToPot[name];
+        if(pot.buyIn == 0) throw; // no pot
+        if(pot.loser != 0) throw; // pot is over
+        if(pot.players.length == 3) throw; // pot is full but waiting for a loser
+
+        if(pot.players[0] != msg.sender && pot.players[1] != msg.sender) throw; // no such player in pot
+        
+        address[] newPlayers;
+        for(uint i = 0; i < pot.players.length; i++){
+            if(pot.players[i] != msg.sender){
+                newPlayers.push(pot.players[i]);
+            }
+        }
+       
+        pot.players = newPlayers;
+
+        msg.sender.send((pot.buyIn * 99) / 100); // leaving fee 1%
+    }*/
+
     function solvePot(string name){
         Pot pot = nameToPot[name];
         if(pot.loser != 0) throw; // pot is over
         if(pot.players.length < 3) throw; // pot not full
-        if(block.number - 256 > pot.lastPlayerBlockNumber) throw; // pot expired - players didn't solve pot - money stays (this is due to hash storage limits - http://solidity.readthedocs.io/en/latest/units-and-global-variables.html#block-and-transaction-properties)
         if(block.number <= pot.lastPlayerBlockNumber) throw;
+        
+        bytes32 blockHash = block.blockhash(pot.lastPlayerBlockNumber + 1);
+        if(blockHash == 0) { // pot expired due to hash storage limits - players didn't solve pot
+            pot.loser = msg.sender; // doesn't matter, everybody loses a fee
+            
+            for(uint i = 0; i < pot.players.length; i++){
+                pot.players[i].send((pot.buyIn * 99) / 100); // return money minus 1% fee
+            }
 
-uint8(uint256(sha3(block.blockhash(lastJoinerBlockNumber+1)))) % 3 == loser index
+            return;
+        }
+
+        bytes32 potShaResult = sha3(msg.sender, blockHash);
+        uint8 loserIndex = uint8(uint256(potShaResult) % 3);
+
+        address winner1 = pot.players[(loserIndex + 1) % 3];
+        address winner2 = pot.players[(loserIndex + 2) % 3];
+
+        uint winAmount = ((pot.buyIn * / 2) * 99) / 100;
+        uint returnAmount = pot.buyIn + winAmount;
+
+        winner1.send(returnAmount);
+        winner2.send(returnAmount);
     }
 }
