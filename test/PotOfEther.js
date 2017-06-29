8c3a3e9ec9964c5532e9e4e4dbc2afefe0bd3e67
assafmo <assaf.morami@gmail.com>
2017-06-29 22:06:24 +0300

    test buyin and name on pot creation

--- test/PotOfEther.js
+++ test/PotOfEther.js
@@ -1,32 +1,62 @@
 var PotOfEther = artifacts.require('./PotOfEther.sol');
 
 contract('PotOfEther', accounts => {
-  it('should emit LogPotCreated event on pot creation', async () => {
-    var instance = await PotOfEther.deployed();
+  describe('createPot', () => {
+    it('should pay buy in > 0', async () => {
+      var instance = await PotOfEther.deployed();
 
-    var result = await instance.createPot('banana', {
-      from: accounts[0],
-      value: 1000
+      try {
+        await instance.createPot('pot1', {
+          from: accounts[0],
+          value: 0
+        });
+        assert.equal(true, false);
+      } catch (err) {
+        assert.equal(true, true);
+      }
     });
 
-    assert.equal(result.logs.length, 2);
-    assert.equal(result.logs[0].args.name, 'banana');
-    assert.equal(result.logs[0].args.buyIn.valueOf(), 1000);
-    assert.equal(result.logs[0].args.firstPlayer, accounts[0]);
-  });
+    it('should pass a name with name.length > 0', async () => {
+      var instance = await PotOfEther.deployed();
 
-  it('should emit LogPotJoin event on pot creation', async () => {
-    var instance = await PotOfEther.deployed();
-
-    var result = await instance.createPot('papaya', {
-      from: accounts[0],
-      value: 1000
+      try {
+        await instance.createPot('pot1', {
+          from: accounts[0],
+          value: 1000
+        });
+        assert.equal(true, false);
+      } catch (err) {
+        assert.equal(true, true);
+      }
     });
 
-    assert.equal(result.logs.length, 2);
-    assert.equal(result.logs[1].args.name, 'papaya');
-    assert.equal(result.logs[1].args.newPlayer, accounts[0]);
-  });
+    it('should emit LogPotCreated event', async () => {
+      var instance = await PotOfEther.deployed();
+
+      var result = await instance.createPot('banana', {
+        from: accounts[0],
+        value: 1000
+      });
+
+      assert.equal(result.logs.length, 2);
+      assert.equal(result.logs[0].args.name, 'banana');
+      assert.equal(result.logs[0].args.buyIn.valueOf(), 1000);
+      assert.equal(result.logs[0].args.firstPlayer, accounts[0]);
+    });
+
+    it('should emit LogPotJoin event', async () => {
+      var instance = await PotOfEther.deployed();
+
+      var result = await instance.createPot('papaya', {
+        from: accounts[0],
+        value: 1000
+      });
+
+      assert.equal(result.logs.length, 2);
+      assert.equal(result.logs[1].args.name, 'papaya');
+      assert.equal(result.logs[1].args.newPlayer, accounts[0]);
+    });
+  })
 });
 
 
